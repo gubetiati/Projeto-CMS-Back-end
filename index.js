85f007823e0bb508441d10c95b1d86272e2cf4cd
gubetiati <gubetiati@gmail.com>
2024-04-20 17:15:27 -0300

    correcao arquivo dotenv

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const session = require('express-session')
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
+const mustacheExpress = require('mustache-express');
+const path = require('path')
 
 dotenv.config()
+console.log(process.env);
+
+
+
+const engine = mustacheExpress()
 const app = express()
 
 //configurações de middleware
@@ -14,14 +21,35 @@ app.use(session({
     saveUninitialized: true
 }));
 
+// Função de middleware para autenticação
+function authenticate(req, res, next) {
+    if (req.session.authenticated) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+// Configuração do mecanismo de template Mustache
+app.engine('mustache', engine);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
+
 // Rota para renderizar o formulário de login
 app.get('/login', (req, res) => {
     res.render('login', {}); // Renderiza a página de login
 });
 
-
-
-
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if (username === process.env.USER && password === process.env.PASSWORD) {
+        req.session.authenticated = true;
+        res.redirect('/admin');
+        
+    } else {
+        res.send('Credenciais inválidas');
+    }
+});
 
 
 
